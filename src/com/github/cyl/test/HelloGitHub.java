978a410654c4098db61e68c90eece7a91ef16a0f
chenyiliang <chenyiliangconan@qq.com>
2015-05-16 21:42:14 +0800

    上传测试成功

--- src/com/github/cyl/test/HelloGitHub.java
+++ src/com/github/cyl/test/HelloGitHub.java
@@ -4,6 +4,7 @@ public class HelloGitHub {
 
 	public static void main(String[] args) {
 		System.out.println("GitHub, I'm coming!");
+		System.out.println("Upload to GitHub successfully!");
 	}
 
 }
